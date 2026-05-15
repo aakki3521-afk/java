3b00656e8dcefd1964b7e702f003b0cea6a29219
aakki3521-afk <aakki3521@gmail.com>
2026-05-15 23:45:29 +0530

    Add files via upload
    leetcode sum

---
+++ count digits even or odd leetcode.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    public int findNumbers(int[] nums) {
+
+        int evenCount = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+
+            int n = nums[i];
+
+            int count = 0;
+
+            // Count digits
+            while(n > 0) {
+
+                n = n / 10;
+
+                count++;
+            }
+
+            // Check even digits
+            if(count % 2 == 0) {
+
+                evenCount++;
+            }
+        }
+
+        return evenCount;
+    }
+}
